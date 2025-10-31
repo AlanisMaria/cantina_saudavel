e975e1e4cdf5f299471d028a18859d580c7d9251
Alanis <160151740+AlanisMaria@users.noreply.github.com>
2025-10-31 12:26:06 -0300

    Add files via upload

---
+++ cantina-back/src/main/java/com/escola/cantina/model/Pedidos.java
@@ -0,0 +1,5 @@
+package main.java.com.escola.cantina.model;
+
+public class Pedidos {
+    
+}
---
+++ cantina-back/src/main/java/com/escola/cantina/repository/PedidosRepository.java
@@ -0,0 +1,5 @@
+package main.java.com.escola.cantina.repository;
+
+public class PedidosRepository {
+    
+}
